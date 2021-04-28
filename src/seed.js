9b153afd1f62ba21abca5ac423bb8c4fc5cc2f1a
TerrapinStat10n <75049442+TerrapinStat10n@users.noreply.github.com>
2021-04-28 16:09:56 -0400

    version 1

---
+++ src/seed.js
@@ -0,0 +1,54 @@
+import shortid from "shortid";
+
+export default function seed(store) {
+  console.log("Insert first list");
+  const firstListId = shortid.generate();
+
+  store.dispatch({
+    type: "ADD_LIST",
+    payload: { listId: firstListId, listTitle: "List Title Goes Here" }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: firstListId,
+      cardId: shortid.generate(),
+      cardText: "Cards can be added or deleted"
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: firstListId,
+      cardId: shortid.generate(),
+      cardText: "They can also be moved around"
+    }
+  });
+
+    const secondListId = shortid.generate();
+
+  store.dispatch({
+    type: "ADD_LIST",
+    payload: { listId: secondListId, listTitle: "List #2" }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: secondListId,
+      cardId: shortid.generate(),
+      cardText: 'Try clicking "Add a card" or "Add a list"'
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: secondListId,
+      cardId: shortid.generate(),
+      cardText: "If you hover over a card, you'll see a pencil icon for deletion"
+    }
+  });
+};
